76d7eefb2ba0ba87705b15a0ce70c911444b1d0f
AhmedAbdallah69 <aljwkrl@gmail.com>
2023-12-11 21:27:10 +0200

    k

--- error.c
+++ error.c
@@ -12,6 +12,18 @@ void printerror(int error, char **command)
 		_puts(command[0]);
 		_puts(": command not found\n");
 	}
+	if (error == 126)
+	{
+		_puts(command[0]);
+		_puts(": Required key not available\n");
+		exit(error);
+	}
+	if (error == 127)
+	{
+		_puts(command[0]);
+		_puts(": Key has expired\n");
+		exit(error);
+	}
 	if (error == 1)
 	{
 		exit(EXIT_SUCCESS);
